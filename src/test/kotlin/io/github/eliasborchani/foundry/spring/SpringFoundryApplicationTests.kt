613873b3c7beb53fee12f2ecf15f36c3c80875da
Elias Borchani <elias.borchani@withings.com>
2025-12-16 16:58:43 +0100

    project: init project

---
+++ src/test/kotlin/io/github/eliasborchani/foundry/spring/SpringFoundryApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.eliasborchani.foundry.spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringFoundryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
